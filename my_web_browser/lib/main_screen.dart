aa14e1a97072372e82618791beeb9041175c94a0
AsherKim <tawans@naver.com>
2023-06-15 02:23:24 +0900

    :sparkles: Feat: Add MyWebView

--- my_web_browser/lib/main_screen.dart
+++ my_web_browser/lib/main_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -8,12 +11,62 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  late WebViewController _webViewController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    if (Platform.isAndroid) WebView.platform = AndroidWebView();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('나만의 웹브라우저'),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('나만의 웹브라우저'),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.add),
+            ),
+            PopupMenuButton<String>(
+                onSelected: (value) {
+                  setState(() {
+                    _webViewController.loadUrl(value);
+                  });
+                },
+                itemBuilder: (context) => [
+                      const PopupMenuItem<String>(
+                        value: 'https://www.google.com',
+                        child: Text('구글'),
+                      ),
+                      const PopupMenuItem<String>(
+                        value: 'https://www.naver.com',
+                        child: Text('네이버'),
+                      ),
+                      const PopupMenuItem<String>(
+                        value: 'https://www.kakao.com',
+                        child: Text('카카오'),
+                      ),
+                    ])
+          ],
+        ),
+        body: WillPopScope(
+          onWillPop: () async {
+            if (await _webViewController.canGoBack()) {
+              await _webViewController.goBack();
+              return false;
+            }
+            return true;
+          },
+          child: WebView(
+            initialUrl: 'https://flutter.dev',
+            javascriptMode: JavascriptMode.unrestricted,
+            onWebViewCreated: (controller) {
+              _webViewController = controller;
+            },
+          ),
+        ));
   }
 }
