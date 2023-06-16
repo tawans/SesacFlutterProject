b9a0f3f11251862b5f1bc9c2d9cf9735e4893173
AsherKim <tawans@naver.com>
2023-06-17 02:58:31 +0900

    :tada: Begin and finish Project

---
+++ tilt_sensor/android/app/src/main/kotlin/com/ahserkim/tilt_sensor/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ahserkim.tilt_sensor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ tilt_sensor/lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:sensors_plus/sensors_plus.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const SensorApp(),
+    );
+  }
+}
+
+class SensorApp extends StatelessWidget {
+  const SensorApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // 가로모드
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeLeft,
+    ]);
+
+    final centerX = MediaQuery.of(context).size.width / 2 - 50;
+    final centerY = MediaQuery.of(context).size.height / 2 - 50;
+
+    return Scaffold(
+      body: Stack(
+        children: [
+          StreamBuilder<AccelerometerEvent>(
+              stream: accelerometerEvents,
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+
+                final event = snapshot.data!;
+                List<double> accelerometerValues = [event.x, event.y, event.z];
+                print(accelerometerValues);
+
+                return Positioned(
+                  left: centerX + event.y * 20,
+                  top: centerY + event.x * 20,
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      color: Colors.green,
+                      shape: BoxShape.circle,
+                    ),
+                    width: 100,
+                    height: 100,
+                  ),
+                );
+              }),
+        ],
+      ),
+    );
+  }
+}
