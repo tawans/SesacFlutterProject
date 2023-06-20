ef33a8f1bc17e96b8631d3bd26b309ddada23dc9
AsherKim <tawans@naver.com>
2023-06-21 05:31:02 +0900

    :tada: Begin and first finished Project

--- youtube_ui_clone/lib/tab/home/home_page.dart
+++ youtube_ui_clone/lib/tab/home/home_page.dart
@@ -50,42 +50,72 @@ class HomePage extends StatelessWidget {
           ],
         ),
       ),
-      body: ListView.builder(
-        itemCount: 3, // 예시로 10개의 아이템을 표시
-        itemBuilder: (context, index) {
-          return Container(
-            padding: const EdgeInsets.all(5.0),
-            child: Stack(
-              children: [
-                Image.asset(
-                  'assets/images/video_3.png', // 이미지 경로
-                  fit: BoxFit.cover,
-                  height: 220,
-                ),
-                ListTile(
-                  contentPadding: const EdgeInsets.only(top: 220),
-                  leading: ClipOval(
-                    child: Image.asset(
-                      // fit: BoxFit.cover,
-                      'assets/images/jjang.jpg',
-                      fit: BoxFit.cover,
-                      width: 50,
-                    ),
+      body: Column(
+        children: [
+          Container(
+            color: Colors.black,
+            height: 40,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: categories.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: ChoiceChip(
+                    label: Text(categories[index]),
+                    selected: index == 0,
+                    onSelected: (selected) {},
                   ),
-                  // leading: const CircleAvatar(
-                  //   backgroundImage:
-                  //       AssetImage('assets/images/character.png'), // 아바타 이미지
-                  // ),
-                  title: Text('돌아온 씨야 ${index + 1}',
-                      style: const TextStyle(fontSize: 15)), // 아이템의 제목
-                  subtitle: const Text('기묘한 캐릭터ㆍ조회수 4만회ㆍ9시간 전',
-                      style: TextStyle(fontSize: 11)), // 아이템의 부제목
-                  trailing: const Icon(Icons.more_vert), // 아이템 우측에 표시될 아이콘
-                ),
-              ],
+                );
+              },
             ),
-          );
-        },
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: 3,
+              itemBuilder: (context, index) {
+                return Container(
+                  color: Colors.black,
+                  padding: const EdgeInsets.all(5.0),
+                  child: Stack(
+                    children: [
+                      Image.asset(
+                        'assets/images/video_3.png',
+                        fit: BoxFit.cover,
+                        height: 220,
+                      ),
+                      ListTile(
+                        contentPadding: const EdgeInsets.only(top: 220),
+                        leading: ClipOval(
+                          child: Image.asset(
+                            'assets/images/jjang.jpg',
+                            fit: BoxFit.cover,
+                            width: 50,
+                          ),
+                        ),
+                        title: Text(
+                          '돌아온 씨야 ${index + 1}',
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 15,
+                          ),
+                        ),
+                        subtitle: const Text(
+                          '기묘한 캐릭터ㆍ조회수 4만회ㆍ9시간 전',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 11,
+                          ),
+                        ),
+                        trailing: const Icon(Icons.more_vert),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
