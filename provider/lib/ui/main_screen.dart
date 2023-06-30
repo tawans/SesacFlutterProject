46d2a4a30f5b32d1ea56349a97cd5aae057f2002
AsherKim <tawans@naver.com>
2023-06-30 10:01:07 +0900

    🎉 Begin a Project

---
+++ provider/lib/ui/main_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_example/ui/main_view_model.dart';
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final viewModel = context.watch<MainViewModel>();
+    return Scaffold(
+      appBar: AppBar(title: const Text('카운터')),
+      body: Center(
+        child: Text(
+          '${viewModel.count}',
+          style: Theme.of(context).textTheme.displayLarge,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          viewModel.increment();
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
