96ff1c0fb56d4b2ba7b6f1c247e07073feb5b78d
AsherKim <tawans@naver.com>
2023-08-08 02:17:41 +0900

    :fire: fix a code

--- karong_dday/lib/main.dart
+++ karong_dday/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPujrple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       home: const MyHomePage(),
